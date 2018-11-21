623a52accc53d941f42b4bddcf90207359be1e14
Luggist <1810653445@stud.fh-kufstein.ac.at>
2018-11-21 21:19:07 +0100

    j

---
+++ src/Aufgabe3_181065445.java
@@ -0,0 +1,18 @@
+public class Aufgabe3_181065445 {
+
+    public static void main(String[] args) {
+
+        int zahl1 = 15;
+        int zahl2 = 25;
+        int zahl3 = 45;
+        double double1 = 13.34;
+        double double2 = 13.35;
+        double double3 = 42.34;
+
+        System.out.println(zahl1/zahl3);
+        System.out.println(double1*zahl2);
+        System.out.println();
+        System.out.println();
+    }
+
+}
