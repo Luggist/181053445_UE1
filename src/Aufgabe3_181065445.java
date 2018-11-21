18303c1fc50f703d316fedc04e1c57a6985567c9
Luggist <1810653445@stud.fh-kufstein.ac.at>
2018-11-21 21:27:10 +0100

    ue3

--- src/Aufgabe3_181065445.java
+++ src/Aufgabe3_181065445.java
@@ -9,10 +9,12 @@ public class Aufgabe3_181065445 {
         double double2 = 13.35;
         double double3 = 42.34;
 
-        System.out.println(zahl1/zahl3);
+        System.out.println(zahl1+zahl2);
+        System.out.println(zahl1/zahl2);
         System.out.println(double1*zahl2);
-        System.out.println();
-        System.out.println();
+        System.out.println(zahl1%zahl2);
+        System.out.println(++zahl3);
+        System.out.println(--zahl2);
     }
 
 }
